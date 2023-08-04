c80e931449fe2fea0aa6d0dc1d77eb232cbb5cdf
Abhishek Sharma <abhishekalien09@gmail.com>
2023-08-04 19:19:19 +0530

    Lets Start
    Integrate tailwind+daisyUI in frontend

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import { Pets } from "./components/pets/Pets";
 
 const App = () => {
-  return <Pets />;
+  return (
+    <>
+      <h1 className="text-4xl underline">Let the fun begin !!</h1>
+    </>
+  );
 };
 
 export default App;
--- react-app/src/components/pets/Pets.jsx
+++ react-app/src/components/pets/Pets.jsx
@@ -1,17 +1,10 @@
-import React, { useState, useEffect } from "react";
-import { findPets } from "../../services/PetServices";
+import React, { useState } from "react";
 import styles from "./Pets.module.css";
 
 export const Pets = () => {
     const [pets, setPets] = useState([]);
 
-    useEffect(() => {
-      findPets()
-            .then(({data}) => {
-            setPets(data);
-            });
-    }, []);
-  return (
+    return (
     <>
         { pets.map(pet => 
         <div className={styles.pets}>
