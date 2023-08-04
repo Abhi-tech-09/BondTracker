76cbb27834a6042aaa02a48da2e68c72e39b5282
Abhishek Sharma <abhishekalien09@gmail.com>
2023-08-04 20:29:23 +0530

    Update pom.xml (set CI=false)

--- react-app/src/components/pets/Pets.jsx
+++ react-app/src/components/pets/Pets.jsx
@@ -1,18 +1,11 @@
-import React, { useState } from "react";
-import styles from "./Pets.module.css";
+import React from "react";
 
 export const Pets = () => {
-    const [pets, setPets] = useState([]);
 
     return (
     <>
-        { pets.map(pet => 
-        <div className={styles.pets}>
-            <div>ID: {pet.id}</div>
-            <div>Name: {pet.name} </div>
-            <div>Age: {pet.age}</div>
-        </div>) 
-        }
+        
+        
     </>
   )
 };
